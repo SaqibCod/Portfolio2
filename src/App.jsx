8de8a7e8c50bbdca6daf91e6339113460e30eb0d
SaqibCod <saqibbca2021@gmail.com>
2025-12-17 13:20:15 +0530

    fix hero error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import styles from './App.module.css';
 import { Navbar } from './components/Navbar/Navbar';
-import { Hero } from './components/Hero/hero';
+import { Hero } from './components/Hero/Hero';
 import { About } from './components/About/About';
-import { Experience } from './components/Experience/Experience';
 import { Projects } from './components/Projects/Projects';
 import { Contact } from './components/Contact/Contact';
 
